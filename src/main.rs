2471941e84f3d7354e54a6a72bfdc72283196370
abyss <13427508433@163.com>
2021-06-24 20:12:09 +0800

    00 base code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use winit::event::{Event, VirtualKeyCode, ElementState,KeyboardInput, WindowEvent};
+use winit::event_loop::{EventLoop, ControlFlow};
+
+const WINDOW_TITLE: &str = "00 base code";
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 600;
+
+struct App;
+
+impl App {
+    fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
+        winit::window::WindowBuilder::new()
+            .with_title(WINDOW_TITLE)
+            .with_inner_size(winit::dpi::LogicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT))
+            .build(event_loop)
+            .expect("Failed to create window.")
+    }
+
+    pub fn main_loop(event_loop: EventLoop<()>) {
+        event_loop.run(|event, _, control_flow| {
+            match event {
+                Event::WindowEvent { event, ..} => {
+                    match event {
+                        WindowEvent::CloseRequested => {
+                            *control_flow = ControlFlow::Exit;
+                        },
+                        WindowEvent::KeyboardInput { input, ..} => {
+                            match input {
+                                KeyboardInput { virtual_keycode, state, .. } => {
+                                    match (virtual_keycode, state) {
+                                        (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
+                                            dbg!("按下Esc");
+                                            *control_flow = ControlFlow::Exit;
+                                        },
+                                        _ => ()
+                                    }
+                                }
+                            }
+                        },
+                        _ => ()
+                    }
+                },
+                _ => (),
+            }
+        })
+    }
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let _window = App::init_window(&event_loop);
+
+    App::main_loop(event_loop);
 }
