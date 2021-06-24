4abf695703382d514b2ca349f533d15c21804e12
abyss <13427508433@163.com>
2021-06-24 21:03:38 +0800

    创建vk instance

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,74 @@
 use winit::event::{Event, VirtualKeyCode, ElementState,KeyboardInput, WindowEvent};
 use winit::event_loop::{EventLoop, ControlFlow};
 
-const WINDOW_TITLE: &str = "00 base code";
+use ash::{vk, version::EntryV1_0, version::InstanceV1_0};
+use std::ffi::CString;
+use std::ptr;
+
+const WINDOW_TITLE: &str = "01 instance creation";
 const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 600;
 
-struct App;
+pub const APPLICATION_VERSION: u32 = 1;
+pub const ENGINE_VERSION: u32 = 1;
+
+struct App {
+    entry: ash::Entry,
+    instance: ash::Instance,
+}
 
 impl App {
+    pub fn new() -> App {
+        let entry = unsafe { ash::Entry::new().unwrap() };
+        let instance = App::create_vk_instance(&entry);
+
+        App {
+            entry: entry,
+            instance: instance
+        }
+    }
+
+    fn create_vk_instance(entry: &ash::Entry) -> ash::Instance{
+        let app_name = CString::new(WINDOW_TITLE).unwrap();
+        let engine_name = CString::new("Vulkan").unwrap();
+        let app_info = vk::ApplicationInfo {
+            s_type: vk::StructureType::APPLICATION_INFO,
+            p_next: ptr::null(),
+            p_application_name: app_name.as_ptr(),
+            application_version: APPLICATION_VERSION,
+            p_engine_name: engine_name.as_ptr(),
+            engine_version: ENGINE_VERSION,
+            api_version: vk::API_VERSION_1_0
+        };
+
+        // let extension_names = 
+
+        let instance_create_info = vk::InstanceCreateInfo {
+            s_type: vk::StructureType::INSTANCE_CREATE_INFO,
+            p_next: ptr::null(),
+            p_application_info: &app_info,
+            pp_enabled_layer_names: ptr::null(),
+            enabled_layer_count: 0,
+            pp_enabled_extension_names: ptr::null(),
+            enabled_extension_count: 0,
+        };
+
+        unsafe {
+            entry.create_instance(&instance_create_info, None)
+                .expect("Failed to create instance")
+        }
+    }
+
     fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
         winit::window::WindowBuilder::new()
             .with_title(WINDOW_TITLE)
             .with_inner_size(winit::dpi::LogicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT))
             .build(event_loop)
             .expect("Failed to create window.")
+    }
+
+    pub fn init_vulkan() {
+
     }
 
     pub fn main_loop(event_loop: EventLoop<()>) {
@@ -45,12 +100,8 @@ impl App {
         })
     }
 
-    pub fn init_vulkan() {
-
-    }
-
     pub fn clean_up() {
-        
+
     }
 }
 
