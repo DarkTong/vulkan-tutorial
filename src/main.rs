215166ff3c35ba10f5eeecf983f58b4017857cef
abyss <13427508433@163.com>
2021-06-30 20:17:15 +0800

    04 logical device

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use ash::vk::Handle;
-use winit::event::{Event, VirtualKeyCode, ElementState,KeyboardInput, WindowEvent};
-use winit::event_loop::{EventLoop, ControlFlow};
+use winit::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::Window;
 
-use ash::{vk, version::EntryV1_0, version::InstanceV1_0};
-use std::ffi::{CStr, CString, c_void};
+use ash::version::{DeviceV1_0, EntryV1_0, InstanceV1_0};
+use ash::vk;
+use std::ffi::{c_void, CStr, CString};
 use std::ptr;
 use std::str::from_utf8;
 
@@ -42,9 +43,8 @@ unsafe extern "system" fn vulkan_debug_utils_debug(
     message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
     message_type: vk::DebugUtilsMessageTypeFlagsEXT,
     p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
-    p_use_data: *mut c_void
-) -> vk::Bool32{
-
+    p_use_data: *mut c_void,
+) -> vk::Bool32 {
     let message_severity_str = match message_severity {
         vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE => "[Verbose]",
         vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => "[Warning]",
@@ -62,16 +62,17 @@ unsafe extern "system" fn vulkan_debug_utils_debug(
 
     let message = unsafe { CStr::from_ptr((*p_callback_data).p_message) };
 
-    println!("[Debug]{}{}{:?}", message_severity_str, message_type_str, message);
+    println!(
+        "[Debug]{}{}{:?}",
+        message_severity_str, message_type_str, message
+    );
 
     vk::FALSE
 }
 
-pub fn check_validation_layer_support(
-    entry: &ash::Entry,
-    layers: &[&'static str]
-) -> bool {
-    let layer_properties = entry.enumerate_instance_layer_properties()
+pub fn check_validation_layer_support(entry: &ash::Entry, layers: &[&'static str]) -> bool {
+    let layer_properties = entry
+        .enumerate_instance_layer_properties()
         .expect("Failed to enumerate Instance Layers Properties");
 
     for check_layer in layers.iter() {
@@ -82,7 +83,7 @@ pub fn check_validation_layer_support(
             if c_str == *check_layer {
                 found = true;
                 break;
-            }   
+            }
         }
 
         if !found {
@@ -93,30 +94,30 @@ pub fn check_validation_layer_support(
     return true;
 }
 
-fn get_debug_utils_messenger_create_info() 
--> vk::DebugUtilsMessengerCreateInfoEXT {        
+fn get_debug_utils_messenger_create_info() -> vk::DebugUtilsMessengerCreateInfoEXT {
     vk::DebugUtilsMessengerCreateInfoEXT {
         s_type: vk::StructureType::DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
         p_next: ptr::null(),
         flags: vk::DebugUtilsMessengerCreateFlagsEXT::empty(),
-        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE | 
-            vk::DebugUtilsMessageSeverityFlagsEXT::WARNING |
-            vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
-        message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL |
-            vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION |
-            vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+            | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+            | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+        message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+            | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+            | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
         pfn_user_callback: Some(vulkan_debug_utils_debug),
         p_user_data: ptr::null_mut(),
     }
 }
 
-fn get_debug_messenger(create_info: &vk::DebugUtilsMessengerCreateInfoEXT, debug_utils_loader: &ash::extensions::ext::DebugUtils) 
--> vk::DebugUtilsMessengerEXT {
+fn get_debug_messenger(
+    create_info: &vk::DebugUtilsMessengerCreateInfoEXT,
+    debug_utils_loader: &ash::extensions::ext::DebugUtils,
+) -> vk::DebugUtilsMessengerEXT {
     if !VALIDATION_INFO.enable_validation {
         vk::DebugUtilsMessengerEXT::null()
-    }
-    else {
-        let utils_messenger = unsafe { 
+    } else {
+        let utils_messenger = unsafe {
             debug_utils_loader
                 .create_debug_utils_messenger(&create_info, None)
                 .expect("Failed to set up debug messenger!")
@@ -126,17 +127,23 @@ fn get_debug_messenger(create_info: &vk::DebugUtilsMessengerCreateInfoEXT, debug
     }
 }
 
-fn print_physical_device_info(instance: &ash::Instance, p_device: vk::PhysicalDevice) 
-{
-    let p_device_properties = unsafe { 
-        instance.get_physical_device_properties(p_device)
-    };
-    let p_device_features = unsafe {
-        instance.get_physical_device_features(p_device)
-    };
-    let p_device_queue_families = unsafe {
-        instance.get_physical_device_queue_family_properties(p_device)
-    };
+fn get_require_layer_raw_names() -> Vec<*const i8> {
+    if VALIDATION_INFO.enable_validation {
+        VALIDATION_INFO
+            .required_validation_layers
+            .iter()
+            .map(|layer_name| *layer_name as *const str as *const i8)
+            .collect::<Vec<*const i8>>()
+    } else {
+        Vec::new()
+    }
+}
+
+fn print_physical_device_info(instance: &ash::Instance, p_device: vk::PhysicalDevice) {
+    let p_device_properties = unsafe { instance.get_physical_device_properties(p_device) };
+    let p_device_features = unsafe { instance.get_physical_device_features(p_device) };
+    let p_device_queue_families =
+        unsafe { instance.get_physical_device_queue_family_properties(p_device) };
 
     // 输出gpu设备信息
     let device_type = match p_device_properties.device_type {
@@ -154,16 +161,12 @@ fn print_physical_device_info(instance: &ash::Instance, p_device: vk::PhysicalDe
         device_name, p_device_properties.device_id, device_type
     );
 
-    println!(
-        "\tAPI Version: {}",
-        p_device_properties.api_version
-    );
+    println!("\tAPI Version: {}", p_device_properties.api_version);
 
     println!("\tSupport Queue Family: {}", p_device_queue_families.len());
     println!("\t\tQueue Count | Graphics, Compute, Transfer, Sparse Binding");
     for queue_family in p_device_queue_families.iter() {
-        let is_graphics_support = if queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS)
-        {
+        let is_graphics_support = if queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
             "support"
         } else {
             "unsupport"
@@ -173,8 +176,7 @@ fn print_physical_device_info(instance: &ash::Instance, p_device: vk::PhysicalDe
         } else {
             "unsupport"
         };
-        let is_transfer_support = if queue_family.queue_flags.contains(vk::QueueFlags::TRANSFER)
-        {
+        let is_transfer_support = if queue_family.queue_flags.contains(vk::QueueFlags::TRANSFER) {
             "support"
         } else {
             "unsupport"
@@ -197,28 +199,25 @@ fn print_physical_device_info(instance: &ash::Instance, p_device: vk::PhysicalDe
             is_sparse_support
         );
     }
-
-
 }
 
 fn find_queue_family(instance: &ash::Instance, p_device: vk::PhysicalDevice) -> QueueFamilyIndices {
-    let p_device_queue_families = unsafe {
-        instance.get_physical_device_queue_family_properties(p_device)
-    };
-    let queue_family_indices = QueueFamilyIndices {
-        graphics_family: None
+    let p_device_queue_families =
+        unsafe { instance.get_physical_device_queue_family_properties(p_device) };
+    let mut queue_family_indices = QueueFamilyIndices {
+        graphics_family: None,
     };
 
-    let index = 0u32;
+    let mut index = 0u32;
     // 选择设备
     for queue_family in p_device_queue_families.iter() {
         let is_graphics_support = queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS);
         // let is_compute_support = queue_family.queue_flags.contains(vk::QueueFlags::COMPUTE);
         // let is_tranfer_suppoprt = queue_family.queue_flags.contains(vk::QueueFlags::TRANSFER);
         if queue_family.queue_count > 0 {
-            if is_graphics_support 
-                // && is_compute_support 
-                // && is_tranfer_suppoprt
+            if is_graphics_support
+            // && is_compute_support
+            // && is_tranfer_suppoprt
             {
                 queue_family_indices.graphics_family = Some(index);
             }
@@ -227,23 +226,23 @@ fn find_queue_family(instance: &ash::Instance, p_device: vk::PhysicalDevice) -> 
         if queue_family_indices.is_complete() {
             break;
         }
-        
+
         index += 1;
     }
 
     queue_family_indices
 }
 
-fn is_device_suitable(instance: &ash::Instance, p_device: vk::PhysicalDevice) 
--> bool {
+fn is_device_suitable(instance: &ash::Instance, p_device: vk::PhysicalDevice) -> bool {
     let queue_family_indices = find_queue_family(instance, p_device);
-    
+
     return queue_family_indices.is_complete();
 }
 
 fn pick_physic_device(instance: &ash::Instance) -> vk::PhysicalDevice {
     let physical_devices = unsafe {
-        instance.enumerate_physical_devices()
+        instance
+            .enumerate_physical_devices()
             .expect("Failed to enumerate Physical Devices!")
     };
 
@@ -262,29 +261,52 @@ fn pick_physic_device(instance: &ash::Instance) -> vk::PhysicalDevice {
             suitable_device = Some(device);
         }
     }
-    
+
     match suitable_device {
         Some(deivce) => deivce,
-        None => panic!("Failed to find a suitable GPU!")
+        None => panic!("Failed to find a suitable GPU!"),
     }
 }
 
-fn create_logic_device(instance: &ash::Instance, p_device: &vk::PhysicalDevice) -> vk::Device {
-    let queue_family_indices = find_queue_family(instance, p_device);
-
+fn create_logic_device(
+    instance: &ash::Instance,
+    p_device: vk::PhysicalDevice,
+    queue_family_indices: &QueueFamilyIndices,
+) -> ash::Device {
     let queue_priority = [1.0f32];
     let device_queue_ci = vk::DeviceQueueCreateInfo {
         s_type: vk::StructureType::DEVICE_QUEUE_CREATE_INFO,
         p_next: ptr::null(),
         flags: vk::DeviceQueueCreateFlags::empty(),
         queue_family_index: queue_family_indices.graphics_family.unwrap(),
-        queue_count: 1,
-        p_queue_priorities: queue_priority.as_ptr()
+        queue_count: queue_priority.len() as u32,
+        p_queue_priorities: queue_priority.as_ptr(),
     };
 
-    
+    let require_layer_raw_names = get_require_layer_raw_names();
 
+    let device_features = vk::PhysicalDeviceFeatures {
+        ..Default::default()
+    };
 
+    let device_ci = vk::DeviceCreateInfo {
+        s_type: vk::StructureType::DEVICE_CREATE_INFO,
+        p_next: ptr::null(),
+        flags: vk::DeviceCreateFlags::empty(),
+        queue_create_info_count: 1,
+        p_queue_create_infos: &device_queue_ci,
+        enabled_layer_count: require_layer_raw_names.len() as u32,
+        pp_enabled_layer_names: require_layer_raw_names.as_ptr(),
+        enabled_extension_count: 0,
+        pp_enabled_extension_names: ptr::null(),
+        p_enabled_features: &device_features,
+    };
+
+    unsafe {
+        instance
+            .create_device(p_device, &device_ci, None)
+            .expect("Failed to create logical device!")
+    }
 }
 
 pub struct ValidationInfo {
@@ -293,7 +315,7 @@ pub struct ValidationInfo {
 }
 
 struct QueueFamilyIndices {
-    graphics_family: Option<u32>
+    graphics_family: Option<u32>,
 }
 
 impl QueueFamilyIndices {
@@ -309,23 +331,25 @@ struct App {
     entry: ash::Entry,
     instance: ash::Instance,
     physical_device: vk::PhysicalDevice,
-    device: vk::Device, // logic device
+    device: ash::Device, // logic device
+    graphics_queue: vk::Queue,
+
     debug_utils_loader: ash::extensions::ext::DebugUtils,
     debug_utils_messenger: vk::DebugUtilsMessengerEXT,
 }
 
-
-const VALIDATION_INFO: ValidationInfo = ValidationInfo{
+const VALIDATION_INFO: ValidationInfo = ValidationInfo {
     enable_validation: true,
-    required_validation_layers: ["VK_LAYER_KHRONOS_validation"]
+    required_validation_layers: ["VK_LAYER_KHRONOS_validation"],
 };
-
 
 impl App {
     pub fn new() -> App {
         let entry = unsafe { ash::Entry::new().unwrap() };
 
-        if VALIDATION_INFO.enable_validation && !check_validation_layer_support(&entry, &VALIDATION_INFO.required_validation_layers) {
+        if VALIDATION_INFO.enable_validation
+            && !check_validation_layer_support(&entry, &VALIDATION_INFO.required_validation_layers)
+        {
             panic!("validation layers requested, but not avaliable!");
         }
 
@@ -333,21 +357,34 @@ impl App {
         let instance = App::create_vk_instance(&entry, &debug_utils_messenger_ci);
 
         let debug_utils_loader = ash::extensions::ext::DebugUtils::new(&entry, &instance);
-        let debug_utils_messenger = get_debug_messenger(&debug_utils_messenger_ci, &debug_utils_loader);
+        let debug_utils_messenger =
+            get_debug_messenger(&debug_utils_messenger_ci, &debug_utils_loader);
 
         let physical_device = pick_physic_device(&instance);
+
+        let queue_family_indices = find_queue_family(&instance, physical_device);
+
+        let logical_device = create_logic_device(&instance, physical_device, &queue_family_indices);
+
+        let graphics_queue = unsafe {
+            logical_device.get_device_queue(queue_family_indices.graphics_family.unwrap(), 0)
+        };
 
         App {
             entry: entry,
             instance: instance,
             physical_device: physical_device,
+            device: logical_device,
             debug_utils_loader: debug_utils_loader,
             debug_utils_messenger: debug_utils_messenger,
+            graphics_queue: graphics_queue,
         }
     }
 
-    fn create_vk_instance(entry: &ash::Entry, debug_utils_messenger_ci: &vk::DebugUtilsMessengerCreateInfoEXT) 
-    -> ash::Instance{
+    fn create_vk_instance(
+        entry: &ash::Entry,
+        debug_utils_messenger_ci: &vk::DebugUtilsMessengerCreateInfoEXT,
+    ) -> ash::Instance {
         let app_name = CString::new(WINDOW_TITLE).unwrap();
         let engine_name = CString::new("Vulkan").unwrap();
 
@@ -358,15 +395,11 @@ impl App {
             application_version: APPLICATION_VERSION,
             p_engine_name: engine_name.as_ptr(),
             engine_version: ENGINE_VERSION,
-            api_version: vk::API_VERSION_1_0
+            api_version: vk::API_VERSION_1_0,
         };
 
-        let require_validataion_layer_raw_names = VALIDATION_INFO
-            .required_validation_layers
-            .iter()
-            .map(|layer_name| *layer_name as *const str as *const i8 )
-            .collect::<Vec<*const i8>>();
-        
+        let require_validataion_layer_raw_names = get_require_layer_raw_names();
+
         let extension_names = required_extension_names();
 
         let instance_create_info = vk::InstanceCreateInfo {
@@ -379,27 +412,18 @@ impl App {
             },
             flags: vk::InstanceCreateFlags::default(),
             p_application_info: &app_info,
-            pp_enabled_layer_names: if VALIDATION_INFO.enable_validation {
-                require_validataion_layer_raw_names.as_ptr()
-            } else {
-                ptr::null()
-            },
-            enabled_layer_count: if VALIDATION_INFO.enable_validation {
-                require_validataion_layer_raw_names.len() as u32
-            } else {
-                0u32
-            },
+            pp_enabled_layer_names: require_validataion_layer_raw_names.as_ptr(),
+            enabled_layer_count: require_validataion_layer_raw_names.len() as u32,
             pp_enabled_extension_names: extension_names.as_ptr(),
-            enabled_extension_count: extension_names.len() as u32 , 
+            enabled_extension_count: extension_names.len() as u32,
         };
 
         unsafe {
-            entry.create_instance(&instance_create_info, None)
+            entry
+                .create_instance(&instance_create_info, None)
                 .expect("Failed to create instance")
         }
     }
-
-
 
     fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
         winit::window::WindowBuilder::new()
@@ -409,62 +433,53 @@ impl App {
             .expect("Failed to create window.")
     }
 
-    pub fn main_loop(mut self, event_loop: EventLoop<()>, window: Window){
-        event_loop.run(move |event, _, control_flow| {
-            match event {
-                Event::WindowEvent { event, ..} => {
-                    match event {
-                        WindowEvent::CloseRequested => {
+    pub fn main_loop(mut self, event_loop: EventLoop<()>, window: Window) {
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => {
+                    *control_flow = ControlFlow::Exit;
+                }
+                WindowEvent::KeyboardInput { input, .. } => match input {
+                    KeyboardInput {
+                        virtual_keycode,
+                        state,
+                        ..
+                    } => match (virtual_keycode, state) {
+                        (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
+                            dbg!("按下Esc");
                             *control_flow = ControlFlow::Exit;
-                        },
-                        WindowEvent::KeyboardInput { input, ..} => {
-                            match input {
-                                KeyboardInput { virtual_keycode, state, .. } => {
-                                    match (virtual_keycode, state) {
-                                        (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
-                                            dbg!("按下Esc");
-                                            *control_flow = ControlFlow::Exit;
-                                        },
-                                        _ => ()
-                                    }
-                                }
-                            }
-                        },
-                        _ => ()
-                    }
-                },
-                Event::MainEventsCleared => {
-                    window.request_redraw()
-                },
-                Event::RedrawRequested(_window_id) => {
-                    self.draw_frame();
+                        }
+                        _ => (),
+                    },
                 },
                 _ => (),
+            },
+            Event::MainEventsCleared => window.request_redraw(),
+            Event::RedrawRequested(_window_id) => {
+                self.draw_frame();
             }
+            _ => (),
         })
     }
 
     pub fn draw_frame(&mut self) {
         // println!("draw")
     }
-
 }
 
 impl Drop for App {
     fn drop(&mut self) {
         if VALIDATION_INFO.enable_validation {
-            unsafe{
-                self.debug_utils_loader.destroy_debug_utils_messenger(self.debug_utils_messenger, None);
+            unsafe {
+                self.debug_utils_loader
+                    .destroy_debug_utils_messenger(self.debug_utils_messenger, None);
             }
         }
         unsafe {
             self.instance.destroy_instance(None);
         }
-
     }
 }
-
-
 
 fn main() {
     let event_loop = EventLoop::new();
@@ -473,4 +488,3 @@ fn main() {
 
     app.main_loop(event_loop, _window);
 }
- 
